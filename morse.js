decaa072afb3f6174990e89e3653fa38025815e2
KHmamoune <108459491+KHmamoune@users.noreply.github.com>
2023-07-21 00:06:17 +0100

    the basics of the morse traslation page

---
+++ morse.js
@@ -0,0 +1,63 @@
+const input = document.querySelector(".input")
+const output = document.querySelector(".output")
+const change = document.querySelector(".switch")
+const modev = document.querySelector(".modev")
+let mode = "morse"
+const morse = {
+  "a": ".-",
+  "b": "-...",
+  "c": "-.-.",
+  "d": "-..",
+  "e": ".",
+  "f": "..-.",
+  "g": "--.",
+  "h": "....",
+  "i": "..",
+  "j": ".---",
+  "k": "-.-",
+  "l": ".-..",
+  "m": "--",
+  "n": "-.",
+  "o": "---",
+  "p": ".--.",
+  "q": "--.-",
+  "r": ".-.",
+  "s": "...",
+  "t": "-",
+  "u": "..-",
+  "w": ".--",
+  "x": "-..-",
+  "y": "-.--",
+  "z": "--..",
+  " ": "/"
+}
+
+change.addEventListener("click", () => {
+  if(mode == "morse"){
+    mode = "string"
+    modev.textContent = "String to Morse"
+  }else{
+    mode = "morse"
+    modev.textContent = "Morse to String"
+  }
+})
+
+input.addEventListener("click", () => {
+  let i = prompt("input morse")
+  let o = ""
+
+  if(mode == "morse"){
+    i = i.split(" ")
+    for(let j=0; j<i.length; j++){
+      o = o + Object.keys(morse).find(key => morse[key] === i[j])
+    }
+  }else{
+    i = i.split("")
+    for(let j=0; j<i.length; j++){
+      o = o + morse[i[j]] + " "
+    }
+  }
+
+  output.textContent = o
+})
+
